8cb339fead888b21cc3e4a70287f5f9506e9104e
LaDonna Moaney <ladonnamoney@gmail.com>
2019-05-01 23:13:27 -0400

    added all changes for dom manipulation project except for append and prepend

--- js/index.js
+++ js/index.js
@@ -37,6 +37,126 @@ const siteContent = {
   },
 };
 
-// Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+// // Example: Update the img src for the logo
+// let logo = document.getElementById("logo-img");
+// logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+
+            //NAV
+
+const navitems = document.querySelectorAll('a');
+
+console.log(navitems);
+
+navitems[0].textContent = 'Services';
+
+navitems[1].textContent = 'Product';
+
+navitems[2].textContent = 'Vision';
+
+navitems[3].textContent = 'Features';
+
+navitems[4].textContent = 'About';
+
+navitems[5].textContent = 'Contact';
+
+
+              //H1
+const ctah1 = document.querySelector('.cta-text h1');
+
+console.log(ctah1);
+    <h1>​</h1>​
+
+ctah1.setAttribute('style', 'white-space: pre;');
+
+ctah1.textContent = 'DOM \r\n';
+
+ctah1.textContent += 'IS \r\n';
+
+ctah1.textContent += 'AWESOME';
+
+
+          //BUTTON
+
+const button = document.querySelector('.cta-text button');
+
+console.log(button);
+    <button>​</button>​
+
+button.textContent = 'Get Started';
+
+
+      //Code Image
+
+const ctaImg = document.querySelector('#cta-img');
+
+console.log(ctaImg);
+
+ctaImg.src = 'img/header-img.png';
+
+
+      //CONTENT AREA
+
+const h4Tags = document.querySelectorAll('.text-content h4');
+
+console.log(h4Tags);
+
+h4Tags[0].textContent = 'Features';
+
+h4Tags[1].textContent = 'About';
+
+h4Tags[2].textContent = 'Services';
+
+h4Tags[3].textContent = 'Product';
+
+h4Tags[4].textContent = 'Vision';
+
+const pTags = document.querySelectorAll('.text-content p');
+
+console.log(pTags);
+
+pTags[0].textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+pTags[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+pTags[2].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+pTags[3].textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+pTags[4].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+const middleImg = document.querySelector('#middle-img');
+
+console.log(middleImg);
+
+middleImg.src = 'img/mid-page-accent.jpg';
+
+
+          //FOOTER SECTION
+
+const lastH4 = document.querySelector('.contact h4');
+
+console.log(lastH4);
+
+lastH4.textContent = 'Contact';
+
+const contactPTags = document.querySelectorAll('.contact p');
+
+console.log(contactPTags);
+
+contactPTags[0].textContent = '123 Way 456 Street \r\n';
+
+contactPTags[0].textContent += 'Somewhere, USA \r\n';
+
+contactPTags[0].setAttribute('style', 'white-space: pre;');
+
+contactPTags[1].textContent = '1 (888) 888-8888';
+
+contactPTags[2].textContent = 'sales@greatidea.io';
+
+const footerPTag = document.querySelector('footer p');
+
+console.log(footerPTag);
+
+footerPTag.textContent = 'Copyright Great Idea! 2018';
